0544b3e7c2b402e29f9e88ce1c5db66930a2fbae
DvidChavez <chavezdavid0609@gmail.com>
2024-09-05 14:23:52 -0700

    add quiz

--- myscript.js
+++ myscript.js
@@ -1,1 +1,27 @@
 
+
+$(document).ready(function(){
+var nexto =0 ;
+
+  $("#hide").click(function(){
+    $("#intro").hide();
+	$(".shown").toggle();
+  });
+  $("#next").click(function(){
+	  nexto = nexto + 1
+	  
+	  if(nexto == 1){
+		$("#question").hide();
+		$(".shown2").toggle();
+	  }
+	  if(nexto == 2){
+		$("#question2").hide();
+		$(".shown3").toggle();
+	  }
+	  if(nexto == 3){
+		$("#question3").hide();
+		$(".shown4").toggle();
+		$(".shown").toggle();
+	  }
+  });
+});
