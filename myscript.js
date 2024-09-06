7a9897e96e083b36c2d2ebcf32b76949c2615d91
DvidChavez <chavezdavid0609@gmail.com>
2024-09-05 20:29:49 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,27 +1,41 @@
-
-
 $(document).ready(function(){
 var nexto =0 ;
 
   $("#hide").click(function(){
-    $("#intro").hide();
-	$(".shown").toggle();
+  	$("#intro").hide();
+	$(".shown").show("slow");
+	
+	$("#answer").click(function(){
+		$("#answer1").slideDown("slow");
+	});
   });
   $("#next").click(function(){
 	  nexto = nexto + 1
 	  
 	  if(nexto == 1){
 		$("#question").hide();
-		$(".shown2").toggle();
+		$(".shown2").show("slow");
+		
+		$("#answer").click(function(){
+			$("#answer2").slideDown("slow");
+		});
 	  }
 	  if(nexto == 2){
 		$("#question2").hide();
-		$(".shown3").toggle();
+		$(".shown3").show("slow");
+		
+		$("#answer").click(function(){
+			$("#answer3").slideDown("slow");
+		});
 	  }
 	  if(nexto == 3){
 		$("#question3").hide();
-		$(".shown4").toggle();
-		$(".shown").toggle();
+		$(".shown4").show("slow");
+		$("#next").hide();
+		
+		$("#answer").click(function(){
+			$("#answer4").slideDown("slow");
+		});
 	  }
   });
 });
